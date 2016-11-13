6ea11a32ec6c52d14e0c2569072bc1fb4e2ddccb
Jure Sokolic <jure.sokolic@gmail.com>
2016-11-13 18:36:47 -0500

    Demo contains a basic example of how to extract features and apply a generalized edge detector

---
+++ Demo.py
@@ -0,0 +1,76 @@
+from VGG19network import make_VGG16_function
+from Edge import getStats
+import matplotlib.pyplot as plt
+
+def plot_results(savefile, Iorig, Jumps, Curvs, Tors, JumpsH, CurvsH, TorsH, JumpsV, CurvsV, TorsV):
+    plt.subplot(431)
+    plt.title("dim %i sigma %g" % (dim, sigma))
+    plt.imshow(Iorig, cmap='Greys_r')
+    plt.axis('off')
+
+    plt.subplot(434)
+    plt.imshow(JumpsH)
+    plt.axis('off')
+    plt.title('Horizontal  Jumps')
+    plt.subplot(435)
+    plt.imshow(JumpsV)
+    plt.axis('off')
+    plt.title('Vertical Jumps')
+    plt.subplot(436)
+    plt.imshow(Jumps)
+    plt.axis('off')
+    plt.title('Jumps')
+
+    plt.subplot(437)
+    plt.imshow(CurvsH)
+    plt.axis('off')
+    plt.title('Horizontal  Curvs')
+    plt.subplot(438)
+    plt.imshow(CurvsV)
+    plt.axis('off')
+    plt.title('Vertical Curvs')
+    plt.subplot(439)
+    plt.imshow(Curvs)
+    plt.axis('off')
+    plt.title('Curvs')
+
+    plt.subplot(4, 3, 10)
+    plt.imshow(TorsH)
+    plt.axis('off')
+    plt.title('Horizontal  Tors')
+    plt.subplot(4, 3, 11)
+    plt.imshow(TorsV)
+    plt.axis('off')
+    plt.title('Vertical Tors')
+    plt.subplot(4, 3, 12)
+    plt.imshow(Tors)
+    plt.axis('off')
+    plt.title('Tors')
+
+    plt.savefig(savefile, bbox_inches="tight", dpi=300)
+
+
+if __name__ == '__main__':
+    imgname = "mandrill"
+    savename = "Results/" + imgname
+
+    dim = 5
+    sigma = 2
+
+    # initialize conv net
+    theano_VGG19 = make_VGG16_function()
+
+    # list of layers to extract features from
+    # 'input' corresponds to the original RGB image
+    layer_list = ['input', 'pool1', 'pool2', 'pool3']
+
+    features, Iorig = theano_VGG19(imgname + '.jpg', layers=layer_list)
+
+    for l in layer_list:
+        Jumps, Curvs, Tors, JumpsH, CurvsH, TorsH, JumpsV, CurvsV, TorsV = getStats(features[l], dim, sigma=sigma)
+        plot_results(savename + '_' + l + '.png', Iorig, Jumps, Curvs, Tors, JumpsH, CurvsH, TorsH, JumpsV, CurvsV, TorsV)
+
+
+
+
+
